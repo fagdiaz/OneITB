af54b5500c526477b448256ef7a9ae9058590278
Leandro Díaz <leandro.diaz@axiomait.com>
2023-04-08 15:46:12 -0300

    first commit

---
+++ OneITB/GraphQL/Mutation.cs
@@ -0,0 +1,6 @@
+﻿namespace GraphQL.GraphQL
+{
+    public class Mutation
+    {
+    }
+}
---
+++ OneITB/GraphQL/Query.cs
@@ -0,0 +1,17 @@
+﻿using HotChocolate;
+using Microsoft.EntityFrameworkCore;
+using OneItb.Data;
+using OneItb.Entities.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GraphQL.GraphQL
+{
+    public class Query
+    {
+        public async Task<List<User>> GetUsers([Service]OneItbContext context)
+        {
+            return await context.Users.ToListAsync();
+        }
+    }
+}
