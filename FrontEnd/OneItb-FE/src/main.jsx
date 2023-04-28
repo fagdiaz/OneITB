7edd461faa12ffeaa0198f9caccb3b81329b662f
Leandro Díaz <leandro.diaz@axiomait.com>
2023-04-28 19:31:48 -0300

    add apollo conecction

--- FrontEnd/OneItb-FE/src/main.jsx
+++ FrontEnd/OneItb-FE/src/main.jsx
@@ -6,8 +6,15 @@ import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css'
 import './assets/css/normalize.css'
 import './assets/css/styles.css'
 import './assets/css/responsive.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
+const client = new ApolloClient({
+        uri: "https://localhost:44397/",
+        cache : new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
-    <App />  
+    <ApolloProvider client={client}>
+        <App />  
+    </ApolloProvider>
 )
